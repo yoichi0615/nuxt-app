43890ffc1d6e2c31f2f6834e1b9c1a164023e563
yo0615 <kiyokiyoharo@gmail.com>
2022-04-06 10:43:36 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Vuetifyのボタン -->
+  <v-btn>
+    Go!
+  </v-btn>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'New Event'
+    }
+  }
+}
+</script>
